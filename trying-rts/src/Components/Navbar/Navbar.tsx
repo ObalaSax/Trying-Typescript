1ea45efd943ac00674e5c7b2417ac7195bcf1be1
ObalaSax <120820140+ObalaSax@users.noreply.github.com>
2024-11-19 22:24:13 +0300

    First Commit

---
+++ trying-rts/src/Components/Navbar/Navbar.tsx
@@ -0,0 +1,45 @@
+import "./Navbar.css";
+
+import { useState } from "react";
+import Sidenav from "../Sidenav/Sidenav";
+
+function Navbar() {
+  const [openSidenav, setOpenSidenav] = useState(true);
+
+  const handleOpenSidenav = () => {
+    setOpenSidenav(!openSidenav);
+  };
+  return (
+    <div className="navbar">
+      <div className="navbar-container">
+        <div className="navbar-sidebar-and-logo">
+          <button onClick={handleOpenSidenav}>Open</button>
+          <img src="" alt="Logo" />
+        </div>
+        <div className="navbar-links">
+          <ul>
+            <li>
+              <a href="">Home</a>
+            </li>
+            <li>
+              <a href="">About</a>
+            </li>
+            <li>
+              <a href="">Contacts</a>
+            </li>
+            <li>
+              <a href="">Noma Sana</a>
+            </li>
+          </ul>
+        </div>
+        <div className="navbar-authentication">
+          <button>Login</button>
+          <button>Sign-up</button>
+        </div>
+      </div>
+      <Sidenav />
+    </div>
+  );
+}
+
+export default Navbar;
---
+++ trying-rts/src/Components/Sidenav/Sidenav.tsx
@@ -0,0 +1,15 @@
+interface Props {
+  openSidenav: boolean;
+}
+
+function Sidenav() {
+  return (
+    <div className="sidenav">
+      <div className="sidenav-container">
+        <h1>SideNav</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Sidenav;
